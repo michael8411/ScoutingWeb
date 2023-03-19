99f69ae05daf5d8222530a57af6379e494a52baa
michael8411 <arinze8411@gmail.com>
2023-03-18 23:53:34 -0500

    Nav conditionals

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,16 @@
 <script setup>
-import VButton from '../components/Button.vue'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const currentPage = computed(() => route.path);
+console.log(currentPage.value)
 </script>
 
 <template>
     <div id="app">
-        <a href="/Scouting">Scouting</a>
-        <a href="/Login">Login</a>
+        <a v-if = "currentPage == '/Login' " href="/Scouting">Scouting</a>
+        <a v-if = "currentPage == '/Scouting' " href="/Login">Login</a>
+
     </div>
 </template>
